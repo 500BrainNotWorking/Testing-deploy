c2c9252061eaf33d0cedcaaff2b6ae213a6640d6
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2025-02-23 04:53:31 +0000

    Added a method to delete a comment, as well as query to obtaim comments

--- App/controllers/comment.py
+++ App/controllers/comment.py
@@ -2,11 +2,18 @@ from App.models import Comment
 from App.models import Review
 from App.database import db
 
+
+def get_all_comments():
+    return Comment.query.all()
+
+def get_comment(id):
+    return Comment.query.filter_by(ID=id).first()
+
 def create_comment(reviewID, staffID, details):
     new_comment= Comment(reviewID=reviewID, staffID=staffID, details=details) 
 
     if new_comment:
-        existing_review = Review.query.get(reviewId)
+        existing_review = Review.query.get(reviewID)
 
         if existing_review:
             existing_review.comments.append(new_comment)
@@ -17,4 +24,12 @@ def create_comment(reviewID, staffID, details):
             return None
     else:
         return None
- 
+
+
+def delete_comment(comment_id):
+    comment = get_comment(comment_id)
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+    else:
+        return None
