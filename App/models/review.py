edf3448c45ad384863d4bd3d649decafda469623
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-04 10:30:24 -0400

    Final changes to model

--- App/models/review.py
+++ App/models/review.py
@@ -2,15 +2,18 @@ from App.database import db
 from .student import Student
 from .karma import Karma  # Import Karma model
 from datetime import datetime
+from sqlalchemy import func
 
 class Review(db.Model):
     __tablename__ = 'review'
     ID = db.Column(db.Integer, primary_key=True)
     studentID = db.Column(db.Integer, db.ForeignKey('student.ID'))
     createdByStaffID = db.Column(db.Integer, db.ForeignKey('staff.ID'))
-    dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
+    dateCreated = db.Column(db.DateTime, server_default=func.now())
     starRating = db.Column(db.Integer, nullable=False)
     details = db.Column(db.String(400), nullable=False)
+    likes = db.Column(db.Integer, nullable=False, default=0)
+    dislikes = db.Column(db.Integer, nullable=False, default=0)
     comments = db.relationship('Comment', backref='review', lazy=True)
 
     def __init__(self, staff, student, starRating, details):
@@ -18,8 +21,6 @@ class Review(db.Model):
         self.studentID = student.ID
         self.starRating = starRating
         self.details = details
-        self.dateCreated = datetime.now()
-        self.adjustKarma()  # Call adjustKarma when a review is created
 
     def get_id(self):
         return self.ID
@@ -31,36 +32,7 @@ class Review(db.Model):
             "createdByStaffID": self.createdByStaffID,
             "dateCreated": self.dateCreated.strftime("%d-%m-%Y %H:%M"),  # Format the date/time
             "starRating": self.starRating,
+            "likes": self.likes,
+            "dislikes": self.dislikes,
             "details": self.details
         }
-
-    def adjustKarma(self):
-        """Adjust the student's karma based on the star rating of the review."""
-        if self.starRating == 5:
-            points = 5
-        elif self.starRating == 4:
-            points = 3
-        elif self.starRating == 3:
-            points = 1
-        elif self.starRating == 2:
-            points = -1
-        else:
-            points = -3
-
-        # Fetch the student's latest karma entry
-        student = Student.query.get(self.studentID)
-        if not student:
-            return  # No student found, exit
-
-        latest_karma = student.get_karma()
-
-        if latest_karma:
-            # Update existing karma
-            latest_karma.points += points
-        else:
-            # Create new karma record if none exists
-            new_karma = Karma(points=points, studentID=self.studentID)
-            db.session.add(new_karma)
-
-        db.session.commit()  # Save changes
-
